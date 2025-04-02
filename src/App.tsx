168b21dc48b713ccc24ff9ced2a352f8b7a19e2b
Adogo <gonda.ado@gmail.com>
2025-04-02 17:29:27 +0200

    add: fix coloring logic and refetch if word not in api dictionary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import "./App.css";
 
 import Line from "./components/Line";
@@ -7,42 +7,95 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 const WORD_API = "https://random-word-api.herokuapp.com/word?length=5";
 const WORD_EXIST_API = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
+const fetchValidWord = async (): Promise<string> => {
+  while (true) {
+    const wordRes = await fetch(WORD_API);
+    const wordData = await wordRes.json();
+    const word = wordData[0];
+
+    const existRes = await fetch(WORD_EXIST_API + word);
+    const existData = await existRes.json();
+
+    if (Array.isArray(existData)) {
+      console.log(word);
+      return word;
+    }
+  }
+};
+
 function App() {
   const queryClient = useQueryClient();
-  const [guesses, setGuesses] = useState([
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-    ["", "", "", "", ""],
-  ]);
+  const [guesses, setGuesses] = useState(Array(6).fill(Array(5).fill("")));
+
+  const [tilesColors, setTilesColors] = useState(
+    Array(6).fill(Array(5).fill("white"))
+  );
+
   const [guessIndex, setGuessIndex] = useState(0);
   const [letterIndex, setLetterIndex] = useState(0);
   const [allowType, setAllowType] = useState(true);
   const { data: WORD, isLoading: isLoadingWord } = useQuery({
-    queryFn: async () => {
-      const res = await fetch(WORD_API);
-      return res.json();
-    },
+    queryFn: fetchValidWord,
     queryKey: ["word"],
     refetchOnWindowFocus: false,
   });
 
-  const WORD_TO_CHECK =
-    guesses[guessIndex].join("") === ""
-      ? "lllll"
-      : guesses[guessIndex].join("");
+  const WORD_TO_CHECK = guesses[guessIndex].join("");
 
   console.log("correct word is: " + WORD);
 
+  const letterAccurence = useMemo(() => {
+    const result: { [key: string]: number } = {};
+    if (!isLoadingWord) {
+      for (const char of WORD) {
+        result[char] = (result[char] || 0) + 1;
+      }
+    }
+    return result;
+  }, [WORD, isLoadingWord]);
+
   useEffect(() => {
+    const colorTiles = () => {
+      const newColorTiles = [...tilesColors];
+      const newColorTile = [...newColorTiles[guessIndex]];
+
+      const guessLetterFreguency: { [key: string]: number } = {};
+      for (let i = 0; i < newColorTile.length; i++) {
+        if (WORD[i] === guesses[guessIndex][i]) {
+          guessLetterFreguency[WORD[i]] =
+            (guessLetterFreguency[WORD[i]] || 0) + 1;
+          newColorTile[i] = "green-300";
+        }
+      }
+
+      for (let i = 0; i < newColorTile.length; i++) {
+        if (newColorTile[i] === "green-300") continue;
+        if (WORD.includes(guesses[guessIndex][i])) {
+          if (
+            letterAccurence[guesses[guessIndex][i]] >
+            (guessLetterFreguency[guesses[guessIndex][i]] || 0)
+          ) {
+            guessLetterFreguency[guesses[guessIndex][i]] =
+              (guessLetterFreguency[guesses[guessIndex][i]] || 0) + 1;
+            newColorTile[i] = "yellow-300";
+          } else {
+            newColorTile[i] = "gray-300";
+          }
+        } else {
+          newColorTile[i] = "gray-300";
+        }
+      }
+      newColorTiles[guessIndex] = newColorTile;
+      setTilesColors(newColorTiles);
+    };
+
     const checkGuess = () => {
-      if (guesses[guessIndex].join("") === WORD[0].toUpperCase()) {
+      colorTiles();
+      if (guesses[guessIndex].join("") === WORD) {
         setAllowType(false);
         alert("You win");
       } else if (guessIndex === 5) {
-        alert("You lost, word was: " + WORD[0].toUpperCase());
+        alert("You lost, word was: " + WORD);
       }
     };
 
@@ -60,7 +113,7 @@ function App() {
         if (!/^[a-zA-Z]$/.test(e.key)) return;
         const newGuesses = [...guesses];
         const newActualGuess = [...newGuesses[guessIndex]];
-        newActualGuess[letterIndex] = e.key.toUpperCase();
+        newActualGuess[letterIndex] = e.key.toLowerCase();
         newGuesses[guessIndex] = newActualGuess;
         setGuesses(newGuesses);
         setLetterIndex((prev) => prev + 1);
@@ -93,8 +146,10 @@ function App() {
     allowType,
     guessIndex,
     guesses,
+    letterAccurence,
     letterIndex,
     queryClient,
+    tilesColors,
   ]);
 
   if (isLoadingWord) {
@@ -106,13 +161,9 @@ function App() {
       <div className="flex flex-col items-center mt-5 p-10 gap-5">
         <h1 className="text-5xl font-sans">WUURDLE</h1>
         {guesses.map((guess, index) => (
-          <Line
-            key={index}
-            correctWord={WORD[0].toUpperCase()}
-            word={guess}
-            submited={guessIndex > index}
-          />
+          <Line key={index} word={guess} tilesColor={tilesColors[index]} />
         ))}
+        {<div></div>}
       </div>
     </>
   );
--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,36 +1,23 @@
 export default function Line({
   word,
-  correctWord,
-  submited,
+  tilesColor,
 }: {
   word: string[];
-  correctWord: string;
-  submited: boolean;
+  tilesColor: string[];
 }) {
   return (
     <div className="flex gap-5">
       {word.map((letter, index) => {
-        let bgColor = " bg-white";
-        if (submited) {
-          if (correctWord.includes(letter)) {
-            bgColor =
-              correctWord[index] === letter
-                ? " bg-green-300"
-                : " bg-yellow-300";
-          } else {
-            bgColor = " bg-gray-300";
-          }
-        }
-
         return (
           <div
             className={
-              "w-20 h-20 border-2 rounded-lg flex items-center justify-center" +
-              bgColor
+              "w-20 h-20 border-2 rounded-lg flex items-center justify-center transition-colors duration-500 ease-in-out " +
+              "bg-" +
+              tilesColor[index]
             }
             key={index}
           >
-            <p className="text-xl">{letter}</p>
+            <p className="text-xl">{letter.toUpperCase()}</p>
           </div>
         );
       })}
