29e936ed794bbffc3691472635f24a0f078fdabb
Adogo <gonda.ado@gmail.com>
2025-04-02 18:21:36 +0200

    fix: tailwind dynamic styling issue with colors

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,19 +1,27 @@
+type TileColor = "white" | "green-300" | "yellow-300" | "gray-300";
+
 export default function Line({
   word,
   tilesColor,
 }: {
   word: string[];
-  tilesColor: string[];
+  tilesColor: TileColor[];
 }) {
+  const colors = {
+    white: "bg-white",
+    "green-300": "bg-green-300",
+    "yellow-300": "bg-yellow-300",
+    "gray-300": "bg-gray-300",
+  };
+
   return (
     <div className="flex gap-5">
       {word.map((letter, index) => {
         return (
           <div
-            className={
-              "w-20 h-20 border-2 rounded-lg flex items-center justify-center transition-colors duration-500 ease-in-out bg-" +
-              tilesColor[index]
-            }
+            className={`w-20 h-20 border-2 rounded-lg flex items-center justify-center transition-colors duration-500 ease-in-out ${
+              colors[tilesColor[index]]
+            }`}
             key={index}
           >
             <p className="text-xl">{letter.toUpperCase()}</p>
