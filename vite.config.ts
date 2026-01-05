4c49ce3ea739fca46b8c17b4ce0f42fc68f33763
Adogo <gonda.ado@gmail.com>
2026-01-05 10:37:11 +0100

    feat: github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/wuurdle/",
   server: {
     watch: {
       usePolling: true, // Enable polling for file changes
